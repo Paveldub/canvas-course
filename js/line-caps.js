983d72b915301686c3033d0b3da04f33549d4882
Pavel dub <dubstr1@gmail.com>
2020-03-24 15:55:42 +0300

    linecaps canvas

---
+++ js/line-caps.js
@@ -0,0 +1,32 @@
+// // definition
+// window.addEventListener('DOMContentLoaded', () => {
+//   let canvas = document.getElementById('hello-world-canvas')
+//   let context = canvas.getContext('2d')
+
+//   //  first line
+//   context.beginPath()
+//   context.lineCap = 'butt'
+//   context.strokeStyle = 'red'
+//   context.lineWidth = 5
+//   context.moveTo(80, 100)
+//   context.lineTo(200, 100)
+//   context.stroke()
+
+//   // second line
+//   context.beginPath()
+//   context.lineCap = 'round' //increase the width of the line
+//   context.strokeStyle = 'blue'
+//   context.lineWidth = 5
+//   context.moveTo(80, 125)
+//   context.lineTo(200, 125)
+//   context.stroke()
+
+//   // third line
+//   context.beginPath()
+//   context.lineCap = 'square' //increase the width of the line
+//   context.strokeStyle = 'green'
+//   context.lineWidth = 5
+//   context.moveTo(80, 150)
+//   context.lineTo(200, 150)
+//   context.stroke()
+// })
