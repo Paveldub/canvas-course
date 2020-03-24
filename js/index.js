485e5af5f39472f5a221089a2aab4cc2462639eb
Pavel dub <dubstr1@gmail.com>
2020-03-24 14:35:14 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// definition
+let canvas = document.getElementById('hello-world-canvas')
+let context = canvas.getContext('2d')
+
+// // draw rectangle
+// context.fillStyle = 'blue'
+// context.fillRect(10, 30, 40, 30)
+
+// // yellow rectangle
+// context.fillStyle = 'yellow'
+// context.fillRect(50, 80, 40, 30)
