4efe451ff054d82b0102cdb34cddebd2d8f43e50
Pavel dub <dubstr1@gmail.com>
2020-03-25 12:16:26 +0300

    complex shapes with shadows and lineCaps

---
+++ js/complex-shapes.js
@@ -0,0 +1,37 @@
+// // definition
+// window.addEventListener('DOMContentLoaded', () => {
+//   let canvas = document.getElementById('hello-world-canvas')
+//   let context = canvas.getContext('2d')
+
+//   // first object
+//   context.beginPath()
+//   context.strokeStyle = 'red'
+//   context.lineWidth = 10
+//   context.lineCap = 'round'
+//   context.lineJoin = 'round'
+//   context.shadowColor = 'yellow'
+//   context.shadowOffsetX = 10
+//   context.shadowOffsetX = 10
+//   context.shadowBlur = 5
+//   context.moveTo(60, 80)
+//   context.lineTo(160, 80)
+//   context.lineTo(80, 150)
+//   context.lineTo(150, 150)
+//   context.stroke()
+
+//   // second object
+//   context.beginPath()
+//   context.strokeStyle = 'blue'
+//   context.lineWidth = 10
+//   context.lineCap = 'round'
+//   context.lineJoin = 'round'
+//   context.shadowColor = 'red'
+//   context.shadowOffsetX = 10
+//   context.shadowOffsetX = 10
+//   context.shadowBlur = 5
+//   context.moveTo(340, 80)
+//   context.lineTo(240, 80)
+//   context.lineTo(340, 180)
+//   context.lineTo(240, 180)
+//   context.stroke()
+// })
