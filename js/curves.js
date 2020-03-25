496147621e81eb3e37caf514da5cf24b42e0b6a5
Pavel dub <dubstr1@gmail.com>
2020-03-25 12:56:04 +0300

    CURVES canvas

---
+++ js/curves.js
@@ -0,0 +1,35 @@
+// window.addEventListener('DOMContentLoaded', () => {
+//   let canvas = document.getElementById('hello-world-canvas')
+//   let context = canvas.getContext('2d')
+
+//   let radian = Math.PI / 180
+
+//   context.beginPath()
+//   context.strokeStyle = 'blue'
+//   context.lineWidth = 10
+//   context.arc(40, 40, 20, 0 * radian, 180 * radian, false)
+//   context.lineCap = 'round'
+//   context.stroke()
+
+//   context.beginPath()
+//   context.strokeStyle = 'blue'
+//   context.lineWidth = 10
+//   context.arc(80, 40, 20, 180 * radian, 360 * radian, false)
+//   context.lineCap = 'round'
+//   context.stroke()
+
+//   context.beginPath()
+//   context.strokeStyle = 'blue'
+//   context.lineWidth = 10
+//   context.arc(120, 40, 20, 0 * radian, 180 * radian, false)
+//   context.lineCap = 'round'
+//   context.stroke()
+// })
+
+// // arc func get 6 params
+// // 1. x
+// // 2. y
+// // 3. radius (20, 50, 70 and etc...)
+// // 4. start Angle
+// // 5. end Angle
+// // 6. direction
