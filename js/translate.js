6bd0794471284fdda8f3213cda167f326127f95e
Pavel dub <dubstr1@gmail.com>
2020-03-31 14:54:54 +0300

    translate canvas

---
+++ js/translate.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+	let canvas = document.getElementById('hello-world-canvas');
+	let context = canvas.getContext('2d');
+	canvas.style.border = '1px solid #000';
+
+	// original
+	context.fillStyle = 'blue';
+	context.fillRect(50, 50, 160, 160);
+	context.fill();
+
+	// translate
+	context.translate(300, 200);
+
+	// translated
+	context.fillStyle = 'blue';
+	context.fillRect(50, 50, 160, 160);
+	context.fill();
+});
